6aeddb783f882969e0eaeb2519593b4c6e687f82
11968-ship-it <11968@holbertonstudents.com>
2025-09-01 10:56:27 +0000

    all

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
 
+/**
+ * main - prints all single digit numbers of base 10 starting from 0,
+ * without using char type, using only putchar twice
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i = 0;
+while (i < 10)
+{
+putchar(48 + i); // 48 is ASCII code for '0'
+i++;
+}
+putchar('\n');
+return (0);
+}
