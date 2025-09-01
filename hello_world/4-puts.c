5225256de4e1a3e731c536b351ed1cac36677c81
11968-ship-it <11968@holbertonstudents.com>
2025-09-01 09:37:40 +0000

    all

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
-/* ...... comment.......*/
+/**
+ * main - Entry point
+ *
+ * Description: Prints "Programming is like building a multilingual puzzle" using the puts function.
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
