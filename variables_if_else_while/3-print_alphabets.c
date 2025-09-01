930bd7a3468fd63ae619206341a8ce0477000324
11968-ship-it <11968@holbertonstudents.com>
2025-09-01 10:49:36 +0000

    all

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
 
+/**
+ * main - prints the alphabet in lowercase, then in uppercase,
+ * followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char ch;
+ch = 'a';
+while (ch <= 'z')
+{
+putchar(ch);
+ch++;
+}
+ch = 'A';
+while (ch <= 'Z')
+{
+putchar(ch);
+ch++;
+}
+putchar('\n');
+return (0);
+}
