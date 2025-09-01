56480ccde8d86cd9718cf2192ce2c4060866837e
11968-ship-it <11968@holbertonstudents.com>
2025-09-01 11:05:20 +0000

    all

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
 
+/**
+ * main - prints all single-digit numbers of base 10,
+ * separated by ", ", followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i = 0;
+while (i < 10)
+{
+putchar(i + '0');
+if (i != 9)
+{
+putchar(',');
+putchar(' ');
+}
+i++;
+}
+putchar('\n');
+return (0);
+}
