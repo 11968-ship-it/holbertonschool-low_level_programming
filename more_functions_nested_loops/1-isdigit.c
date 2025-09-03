f1070aeac54f42d62f611f68c982dfd1dbd39729
11968-ship-it <11968@holbertonstudents.com>
2025-09-03 20:58:43 +0300

    Update 1-isdigit.c

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,10 +1,16 @@
 #include "main.h"
-\**
-*
-*
-*/
+/**
+ * _isdigit - Checks for a digit
+ * @c: The character to check (as an ASCII value)
+ *
+ * Description: Treturns 1 if the input charac
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
 
 int _isdigit(int c)
 {
-
+if (c == 'A' && c != 'Z')
+return (1);
+else
+return (0);
 }
