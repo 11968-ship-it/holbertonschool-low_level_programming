a608c267bac16df0977d1c51713c8872a684602e
11968-ship-it <11968@holbertonstudents.com>
2025-09-03 12:23:14 +0300

    Update 1-alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -4,7 +4,7 @@
  * print_alphabet - prints the alphabet in lowercase
  * followed by a new line
  */
-void print_alphabet()
+void print_alphabet(void)
 {
 char letter;
 for (letter = 'a'; letter <= 'z'; letter++)
