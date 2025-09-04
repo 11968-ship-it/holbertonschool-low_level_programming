45b00345f9ac806488f2c6f76de86e696bbb03bd
11968-ship-it <11968@holbertonstudents.com>
2025-09-04 10:18:33 +0300

    Update 2-mul.c

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,16 +1,12 @@
 #include "main.h"
 /**
- * _isdigit - Checks for a digit
- * @c: The character to check (as an ASCII value)
- *
+ * mul - Multiplies two iintgers
+ * 
  * Description: Treturns 1 if the input digit
  * Return: 1 if c is digit, 0 otherwise
  */
 
-int _isdigit(int c)
+int mul(int a, int b)
 {
-if (c >= '0' && c <= '9')
-return (1);
-else
-return (0);
+return a * b;
 }
