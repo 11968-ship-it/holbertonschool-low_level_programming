40c0b8e7e1ac51d8ad4ad73e4c393efd36805235
11968-ship-it <11968@holbertonstudents.com>
2025-09-04 11:08:29 +0300

    Update 3-print_numbers.c

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,1 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - Prints the numbers, from 0 to 9, followed by a new line
+ * @c: The character to check (as an ASCII value)
+ *
+ * Description: Tret
+ * Return: ise
+ */
 
+void print_numbers(void)
+{
+if (c >= '0' && c <= '9')
+return (1);
+else
+return (0);
+}
