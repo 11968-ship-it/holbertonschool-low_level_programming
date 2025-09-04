e100d69ca5d2cadeb11da0171b93a1695d35cabc
11968-ship-it <11968@holbertonstudents.com>
2025-09-04 11:48:54 +0300

    Update 4-print_most_numbers.c

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_numbers - Prints the numbers, from 0 to 9, but not 2 and 4
+ * print_most_numbers - Prints the numbers, from 0 to 9, but not 2 and 4
  * Return: void
  */
 
