c18183226bb7debd328433c9e2f12610185cf107
11968-ship-it <11968@holbertonstudents.com>
2025-09-04 11:54:42 +0300

    Update 5-more_numbers.c

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,1 +1,24 @@
+#include "main.h"
+/**
+ * more_numbers - Prints 10 times the numbers from 0 to 14
+ * each followed by a new line.
+ *
+ * Return: Nothing (void)
+ */
 
+void more_numbers(void)
+{
+int i = 0;
+char c;
+while (i < 10)
+{
+c = 'a';
+while (c <= 'z')
+{
+_putchar(c);
+c++;
+}
+_putchar('\n');
+i++;
+}
+}
