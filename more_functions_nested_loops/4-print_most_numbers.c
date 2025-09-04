11e2121f2caeff61e7eae22f43a7c22c5487dbaf
11968-ship-it <11968@holbertonstudents.com>
2025-09-04 11:43:18 +0300

    Update 4-print_most_numbers.c

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,1 +1,18 @@
+#include "main.h"
+/**
+ * print_numbers - Prints the numbers, from 0 to 9, but not 2 and 4
+ * Return: void
+ */
 
+void print_numbers(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+{
+if (i != 2 && i= 4)
+{
+_putchar(i + '0');
+}
+}
+_putchar('\n');
+}
