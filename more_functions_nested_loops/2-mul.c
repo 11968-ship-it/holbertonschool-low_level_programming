90cc6cb619ee399352e108d2a2cea35638d736cf
11968-ship-it <11968@holbertonstudents.com>
2025-09-04 10:12:37 +0300

    Update 2-mul.c

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,1 +1,16 @@
+#include "main.h"
+/**
+ * _isdigit - Checks for a digit
+ * @c: The character to check (as an ASCII value)
+ *
+ * Description: Treturns 1 if the input digit
+ * Return: 1 if c is digit, 0 otherwise
+ */
 
+int _isdigit(int c)
+{
+if (c >= '0' && c <= '9')
+return (1);
+else
+return (0);
+}
