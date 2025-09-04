76cff94750237a52612832355567aad41e99d30f
11968-ship-it <11968@holbertonstudents.com>
2025-09-04 10:27:51 +0300

    Update 2-mul.c

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
  * mul - Multiplies two intgers
- * 
+ *
  * @a: The first integer
  * @b: The second integer
  */
 
 int mul(int a, int b)
 {
-return a * b;
+return (a * b);
 }
