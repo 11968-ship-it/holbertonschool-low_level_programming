eee9c50d8b9e46bd7b6f159933e30f854487f1a2
11968-ship-it <11968@holbertonstudents.com>
2025-09-05 22:52:46 +0300

    Update 4-print_rev.c

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,1 +1,23 @@
+#include "main.h"
 
+/**
+ * print_rev - Prints a string in reverse
+ * @s: Pointer to the string to be printed
+ *
+ * Description: Calculates the length of the string then prints
+ * each character in reverse order
+ */
+void print_rev(char *s)
+{
+int len = 0;
+int i;
+while (s[len] != '\0')
+{
+len++;
+}
+for (i = len - 1; i >= 0; i--)
+{
+_putchar(s[i]);
+}
+_putchar('\n');
+}
