946705fc2b447f6b48a58d668420001941c0bebd
11968-ship-it <11968@holbertonstudents.com>
2025-09-05 22:26:18 +0300

    Update 1-swap.c

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,1 +1,13 @@
+//#include "main.h"
 
+/**
+* reset_to_98 - A function to update the value of integer to 98
+* @n: pointer to int
+*
+* Description: Takes a pointer to an int and updates the value it points to 98
+*/
+
+//void reset_to_98(int *n)
+{
+//*n = 98;
+}
