15cbaefb909dfb52cc84e98e96009c855bb5ca9e
11968-ship-it <11968@holbertonstudents.com>
2025-09-05 22:41:49 +0300

    Update 2-strlen.c

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,1 +1,19 @@
-
+#include "main.h"
+/**
+* _strlen - Returns the length of a string
+ * @s: Pointer to the string
+ *
+ * Description: This function calculates the number of characters
+ * in a string, excluding the null terminator.
+ *
+ * Return: Length of the string
+*/
+int _strlen(char *s)
+{
+int length = 0;
+while (s[length] != '\0')
+{
+length++;
+}
+return (length);
+}
