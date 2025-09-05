66290b52dd4bb8d29b576ee0d7a3166be2ba304c
11968-ship-it <11968@holbertonstudents.com>
2025-09-05 22:44:49 +0300

    Update 3-puts.c

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,1 +1,18 @@
+#include "main.h"
 
+/**
+ * _puts - Prints a string to stdout, followed by a new line
+ * @str: Pointer to the string to be printed
+ *
+ * Description: It loops through each character of the string
+ */
+void _puts(char *str)
+{
+int i = 0;
+while (str[i] != '\0')
+{
+_putchar(str[i]);
+i++;
+}
+_putchar('\n');
+}
