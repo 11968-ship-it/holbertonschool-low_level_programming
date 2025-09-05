f634ba03fef16c1e3db995c497a26d3d24d5ab49
11968-ship-it <11968@holbertonstudents.com>
2025-09-05 23:17:56 +0300

    Update 6-puts2.c

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -18,15 +18,3 @@ _putchar(str[i]);
 }
 _putchar('\n');
 }
-
-
-//void puts2(char *str)
-//{
-//int i = 0;
-//while (str[i] != '\0')
-//{
-//_putchar(str[i]);
-//i += 2;
-//}
-//_putchar('\n');
-//}
