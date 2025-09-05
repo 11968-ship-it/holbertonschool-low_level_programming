39590295803d896ce000ff059f3bb00e9dfbc9ed
11968-ship-it <11968@holbertonstudents.com>
2025-09-05 22:23:13 +0300

    Update 0-reset_to_98.c

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -4,7 +4,7 @@
 * reset_to_98: A function to update the value of integer to 98
 * @n: pointer to int
 *
-* Description: Takes a pointer to an int and updates the value it points to to 98
+* Description: Takes a pointer to an int and updates the value it points to 98
 */
 
 void reset_to_98(int *n)
