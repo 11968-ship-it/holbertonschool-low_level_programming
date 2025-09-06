57a97a6acc7fa64e3897632e08c4ac316584eaf0
11968-ship-it <11968@holbertonstudents.com>
2025-09-06 16:52:45 +0300

    Update 3-strcmp.c

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,1 +1,22 @@
+#include "main.h"
 
+/**
+ * _strcmp - Compares two strings
+ * @s1: The first string to compare
+ * @s2: The second string to compare
+ *
+ * Description: Compares the two strings s1 and s2.
+ * 
+ * Return: An integer difference between the first differing characters.
+ */
+int _strcmp(char *s1, char *s2)
+{
+int i = 0;
+while (s1[i] != '\0' && s2[i] != '\0')
+{
+if (s1[i] != s2[i])
+return (s1[i] - s2[i]);
+i++;
+}
+return (s1[i] - s2[i]);
+}
