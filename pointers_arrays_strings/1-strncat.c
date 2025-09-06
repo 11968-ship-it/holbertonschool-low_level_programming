eec079b6d7f4403e88bb693e4193fe1801935a3a
11968-ship-it <11968@holbertonstudents.com>
2025-09-06 16:48:04 +0300

    Update 1-strncat.c

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,1 +1,28 @@
+#include "main.h"
 
+/**
+ * _strncat - Concatenates two strings using at most n bytes from src.
+ * @dest: The destination string to which src will be appended.
+ * @src: The source string to append.
+ * @n: The maximum number of bytes to append from src.
+ *
+ * Description: This function appends at most `n` bytes from the `src`
+ * string to the end of the `dest` string.
+ *
+ * Return: A pointer to the resulting string dest.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int i = 0;
+int j = 0;
+while (dest[i] != '\0')
+i++;
+while (j < n && src[j] != '\0')
+{
+dest[i] = src[j];
+i++;
+j++;
+}
+dest[i] = '\0';
+return (dest);
+}
