fb4db1683f65f8c3741601c583fdc73631949d03
11968-ship-it <11968@holbertonstudents.com>
2025-09-06 17:04:46 +0300

    Update 7-leet.c

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -1,1 +1,29 @@
+#include "main.h"
 
+/**
+ * leet - Encodes a string into 1337
+ * @s: The string to encode
+ *
+ * Description: To replaces a/A with 4, e/E with 3, o/O with 0, t/T
+ * with 7, and l/L with 1.
+ *
+ * Return: Pointer to the modified string.
+ */
+char *leet(char *s)
+{
+int i, j;
+char letters[] = "aAeEoOtTlL";
+char numbers[] = "4433007711";
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; j < 10; j++)
+{
+if (s[i] == letters[j])
+{
+s[i] = numbers[j];
+break;
+}
+}
+}
+return (s);
+}
