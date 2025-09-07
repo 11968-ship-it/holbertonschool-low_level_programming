b52ec1aab06d8c33991aa2fc3d6625f2463ba150
11968-ship-it <11968@holbertonstudents.com>
2025-09-07 19:00:00 +0300

    Update 0-memset.c

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,4 +1,14 @@
 #include "main.h"
 /**
-
+* _memset - Function fills the first n bytes of the memory area
+ *
+ * Description: This function fills the first n bytes of the memory area
+ * pointed to by s with the constant byte b
+ *
+ * Return: A pointer to the memory area s
 */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+
+}
