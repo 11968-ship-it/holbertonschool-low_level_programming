341adee66e9b32040be9a04641b09e94f8832894
11968-ship-it <11968@holbertonstudents.com>
2025-09-09 18:04:51 +0300

    Update 3-strspn.c

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,17 +1,17 @@
 #include "main.h"
-/**
- * _strspn - Function that gets the length of a prefix substring
- *
- * Description: It gets the hat gets the length of a prefix substring
- *@:
- *@:
- *
- * Return: A Returns the number of bytes in the initial segment of s
- * which consist only of bytes from accept
-*/
 
+/**
+ * _strspn - Gets the length of a prefix substring
+ * @s: The string to be searched
+ * @accept: The characters to match
+ *
+ * Return: The number of bytes in the initial segment of s
+ * which consist only of bytes from accept
+ */
 unsigned int _strspn(char *s, char *accept)
 {
+unsigned int i = 0, j;
+int match;
 while (s[i])
 {
 match = 0;
