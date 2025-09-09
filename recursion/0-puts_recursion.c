940fb19725bd80101ef97928647e8976b4a28095
11968-ship-it <11968@holbertonstudents.com>
2025-09-09 21:01:13 +0300

    Update 0-puts_recursion.c

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
 * _puts_recursion - A function that prints a string
-* @:
+* @s: A pointer to a character
 * Description: The function prints a string, followed by
 * a new line
 * Return:
