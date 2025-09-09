bcef55f85f3b8caae0b4ac5cd074648ea716534a
11968-ship-it <11968@holbertonstudents.com>
2025-09-09 19:07:00 +0300

    Update 7-print_chessboard.c

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -1,1 +1,12 @@
 #include "main.h"
+
+/**
+ * print_chessboard - Function that prints a chessborard
+ *
+ * Description: A Function that prints a chessborard
+ * Return:
+ */
+void print_chessboard(char (*a)[8])
+{
+int board[][];
+}
