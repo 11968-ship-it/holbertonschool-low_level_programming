a2786572a985330368ec4153b25494d025916ca9
11968-ship-it <11968@holbertonstudents.com>
2025-09-09 18:16:50 +0300

    Update 4-strpbrk.c

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -4,14 +4,23 @@
  * _strpbrk - Function that searches a string
  * 
  * Description: Searches a string for any of a set of bytes
- * @
- * @
+ * @s: The string to search
+ * @c: The character to find
  *
  * Return: a pointer to the byte in s that matches one of
  * the bytes in accept, or NULL if no such byte is found
  */
 char *_strpbrk(char *s, char *accept)
 {
-
-return (NULL);
+while (*s)
+{
+if (*s == c);
+return (s);
+s++;
+}
+{
+if (c == ?)
+}
+return (s);
+return (0);
 }
