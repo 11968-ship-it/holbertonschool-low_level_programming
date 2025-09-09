b739dba00082850da770c9cb533bd85dac310c90
11968-ship-it <11968@holbertonstudents.com>
2025-09-09 19:27:06 +0300

    Update 8-print_diagsums.c

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -7,16 +7,14 @@
  * @size: Size of the square matrix
  */
 void print_diagsums(int *a, int size)
-{
-int i, sum1 = 0, sum2 = 0;
-for (i = 0; i < size; i++)
-{
-sum1 += a[i * size + i];
-sum2 += a[i * size + (size - 1 - i)];
+int i;
+int sum_main = 0;
+int sum_anti = 0;
+for (i = 0; i < size; i++) {
+sum_main += *((a + i * size) + i);
+sum_anti += *((a + i * size) + (size - 1 - i));
 }
-print_number(sum1);
-_putchar(',');
-_putchar(' ');
-print_number(sum2);
-_putchar('\n');
+int total = sum_main + sum_anti;
+*a = total;
+}
 }
