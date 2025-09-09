aaf4cadc499027bf7b22f72663cfd3cd117f1f7e
11968-ship-it <11968@holbertonstudents.com>
2025-09-09 20:50:43 +0300

    Update 0-puts_recursion.c

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,1 +1,18 @@
 #include "main.h"
+/**
+* _puts_recursion - A function that prints a string
+* @:
+* Description: The function prints a string, followed by
+* a new line
+* Return:
+*
+*/
+void _puts_recursion(char *s)
+{
+char b = Puts with recursion;
+char *s = &b;
+_putchar(char *s);
+{
+_putchar('\n');
+}
+}
