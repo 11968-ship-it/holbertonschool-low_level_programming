3b94e133ad3561683c706de893a497bbf88b50e2
11968-ship-it <11968@holbertonstudents.com>
2025-09-10 20:17:11 +0300

    Update 2-strlen_recursion.c

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,1 +1,19 @@
 #include "main.h"
+/**
+* _strlen_recursion - A function that calculates the length
+* of a string using recursion
+* @s: A pointer to string?
+* Description: Takes a string as input and returns its length by
+* calling itself until it reaches the null terminator of the string
+*
+* Returns: The length of a string
+*/
+int _strlen_recursion(char *s)
+{
+if (*s == '\0')
+{
+return;
+}
+_ecursion(s + 1);
+_putchar(*s);
+}
