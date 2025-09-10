f9c85f100248afdac2e4daa76c231a995fd01f4a
11968-ship-it <11968@holbertonstudents.com>
2025-09-10 19:45:37 +0300

    Update 1-print_rev_recursion.c

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,1 +1,18 @@
 #include "main.h"
+/**
+* _puts_rev_recursion - A function that prints a string in reverse
+* @s: A pointer to a character
+* Description: The function prints a string in reverse
+* Return:
+*
+*/
+void _puts_rev_recursion(char *s)
+{
+if (*s == '\0')
+{
+_putchar('\n');
+return;
+}
+_puts_rev_recursion(s + 1);
+_putchar(*s);
+}
