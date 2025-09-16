08af3784ced3994c034d61a4faaf6ab1853b0ba9
11968-ship-it <11968@holbertonstudents.com>
2025-09-16 19:45:50 +0300

    Update 3-array_range.c

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -12,12 +12,12 @@ int *array_range(int min, int max)
 int *arr;
 int i, size;
 if (min > max)
-return NULL;
+return (NULL);
 size = max - min + 1;
 arr = malloc(sizeof(int) * size);
 if (arr == NULL)
-return NULL;
+return (NULL);
 for (i = 0; i < size; i++)
 arr[i] = min + i;
-return arr;
+return (arr);
 }
