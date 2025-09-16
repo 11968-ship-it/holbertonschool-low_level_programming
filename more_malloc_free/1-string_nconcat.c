1b97e026ac09342ee8e3c70874cf9fe6147210e5
11968-ship-it <11968@holbertonstudents.com>
2025-09-16 19:24:14 +0300

    Create 1-string_nconcat.c

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _calloc - Allocates memory for an array using calloc.
+ * @nmemb: Number of elements
+ * @size: Size of each element in bytes.
+ *
+ * Return: Pointer to the allocated memory.
+ *If nmemb or size is 0, returns NULL.
+ *If malloc fails, returns NULL.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+void *ptr;
+if (nmemb == 0 || size == 0)
+return (NULL);
+ptr = calloc(nmemb, size);
+if (ptr == NULL)
+return (NULL);
+return (ptr);
+}
