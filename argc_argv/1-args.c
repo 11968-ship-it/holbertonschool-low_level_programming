d5645cd4d0f3838a52c44e62567ab90e8c1819db
11968-ship-it <11968@holbertonstudents.com>
2025-09-16 03:26:55 +0300

    Update 1-args.c

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* main - Printsthe the number of arguments passed into it
+* @argc: Arrguments count
+* @argv: Arrguments vector
+*
+* Return: 0
+*/
 
+int main(int argc __attribute__((unused)), char *argv[])
+{
+printf("%d\n", argc[]);
+return (0);
+}
