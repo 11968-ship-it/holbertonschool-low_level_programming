e5afaf6e6286e75093d8aeeb3a0cb05056a2bf78
11968-ship-it <11968@holbertonstudents.com>
2025-09-16 19:28:01 +0300

    Update 1-string_nconcat.c

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,21 +1,33 @@
 #include <stdlib.h>
+#include <string.h>
 #include "main.h"
 /**
- * _calloc - Allocates memory for an array using calloc.
- * @nmemb: Number of elements
- * @size: Size of each element in bytes.
+ * string_nconcat - Concatenates two strings
+ * @s1: The first string
+ * @s2: The second string
+ * @n: The number of bytes from s2 to concatenate
  *
- * Return: Pointer to the allocated memory.
- *If nmemb or size is 0, returns NULL.
- *If malloc fails, returns NULL.
+ * Return: Pointer to newly allocated space containing s1 followed by
+ * the first n bytes of s2, null-terminated. If function fails,
+ * returns NULL. If NULL is passed, treat it as an empty string
  */
-void *_calloc(unsigned int nmemb, unsigned int size)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-void *ptr;
-if (nmemb == 0 || size == 0)
+unsigned int len1 = 0, len2 = 0, i;
+char *result;
+if (s1 != NULL)
+len1 = strlen(s1);
+if (s2 != NULL)
+len2 = strlen(s2);
+if (n >= len2)
+n = len2;
+result = malloc(len1 + n + 1);
+if (result == NULL)
 return (NULL);
-ptr = calloc(nmemb, size);
-if (ptr == NULL)
-return (NULL);
-return (ptr);
+for (i = 0; i < len1; i++)
+result[i] = s1[i];
+for (i = 0; i < n; i++)
+result[len1 + i] = s2[i];
+result[len1 + n] = '\0';
+return (result);
 }
