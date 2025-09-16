2cd9b10127ebeaefce3d0d1edab2df15ea35b97f
11968-ship-it <11968@holbertonstudents.com>
2025-09-16 03:22:11 +0300

    Update 0-whatsmyname.c

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include "main.h"
 /**
+* main - Prints the name of the program
+* @argc: Arrguments count
+* @argv: Arrguments vector
 *
-*
+* Return: 0
 */
 
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
+printf("%s\n", argv[0]);
 return (0);
 }
