65a2253af350dda9ab90f6688ce5008d384fc8bd
11968-ship-it <11968@holbertonstudents.com>
2025-09-16 19:30:15 +0300

    Update 1-string_nconcat.c

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -13,21 +13,28 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-unsigned int len1 = 0, len2 = 0, i;
-char *result;
-if (s1 != NULL)
-len1 = strlen(s1);
-if (s2 != NULL)
-len2 = strlen(s2);
-if (n >= len2)
-n = len2;
-result = malloc(len1 + n + 1);
-if (result == NULL)
-return (NULL);
-for (i = 0; i < len1; i++)
-result[i] = s1[i];
-for (i = 0; i < n; i++)
-result[len1 + i] = s2[i];
-result[len1 + n] = '\0';
-return (result);
+    unsigned int len1 = 0, len2 = 0, i;
+    char *result;
+
+    if (s1 != NULL)
+        len1 = strlen(s1);
+    if (s2 != NULL)
+        len2 = strlen(s2);
+
+    if (n >= len2)
+        n = len2;
+
+    result = malloc(len1 + n + 1);
+    if (result == NULL)
+        return (NULL);
+
+    for (i = 0; i < len1; i++)
+        result[i] = s1[i];
+
+    for (i = 0; i < n; i++)
+        result[len1 + i] = s2[i];
+
+    result[len1 + n] = '\0';
+
+    return (result);
 }
