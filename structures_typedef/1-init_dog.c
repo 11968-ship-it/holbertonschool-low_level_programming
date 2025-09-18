f799d2610ffe1da2949a8c3a3189761d9eed3fe5
11968-ship-it <11968@holbertonstudents.com>
2025-09-18 20:16:21 +0300

    Update 1-init_dog.c

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -11,10 +11,6 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 typedef struct dog my_dog
 {
-char;
-int;
+return (init_dog(struct dog *d, *name, age));
 }
-return (0);
 }
-//printf(*name, age);
-
