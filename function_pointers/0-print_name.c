6fcdc3c47f6491cf6a0e079a8a6fece8d3da0059
11968-ship-it <11968@holbertonstudents.com>
2025-09-21 17:01:48 +0300

    Update 0-print_name.c

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,1 +1,16 @@
-
+#include "function_pointers.h"
+#include <stddef.h>
+/**
+ * print_name - Calls a function to print a name
+ * @name: The name to print
+ * @f: A pointer to the function to use to print the name
+ *
+ * Description: This function takes a name and a function pointer
+ * as arguments. If neither is NULL, it calls the function
+ * passing the name as its argument.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+if (name != NULL && f != NULL)
+f(name);
+}
