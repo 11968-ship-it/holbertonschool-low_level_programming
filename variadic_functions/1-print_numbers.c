0d23b3bb74ba4ef2240dc926028e6d31cde4f265
11968-ship-it <11968@holbertonstudents.com>
2025-09-25 19:55:01 +0300

    Update 1-print_numbers.c

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -1,1 +1,14 @@
-
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+*
+*
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+if (seaparator != NULL)
+{
+printf(%d);
+return (0);
+}
+}
