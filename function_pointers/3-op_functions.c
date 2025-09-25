e1917fb00aedaad171c977d808c2b5dfe062a093
11968-ship-it <11968@holbertonstudents.com>
2025-09-25 17:46:40 +0300

    Update 3-op_functions.c

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -7,18 +7,24 @@
 */
 int op_add (int a, int b)
 {
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
-int a;
-int b;
-int sum, sub, mul;
-float mod;
-sum = op_add(%d + %d = %d, a, b, a + b);
-sub = op_sub(%d - %d = %d, a, b, a - b);
-mul = op_mul(%d * %d = %d, a, b, a * b);
-mod = op_mod(%d % %d = %f, a, b, a % b);
-return ();
+int op_add(int a, int b)
+{
+return (a + b);
+}
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+int op_div(int a, int b)
+{
+return (a / b);
+}
+int op_mod(int a, int b)
+{
+return (a % b);
+}
 }
