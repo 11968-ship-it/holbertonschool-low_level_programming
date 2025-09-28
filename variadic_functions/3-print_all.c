63a6310f3ced810d989cc52a2c0f312467ce2895
11968-ship-it <11968@holbertonstudents.com>
2025-09-28 13:31:39 +0300

    Update 3-print_all.c

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -17,32 +17,35 @@ void print_all(const char * const format, ...)
 va_list args;
 int i = 0, printed = 0;
 char *str;
-va_start(args, format);
-while (format && format[i])
-{
-if (format[i] == 'c' || format[i] == 'i' ||
-format[i] == 'f' || format[i] == 's')
-{
-if (printed)
-printf(", ");
-printed = 1;
-if (format[i] == 'c')
-printf("%c", va_arg(args, int));
-if (format[i] == 'i')
-printf("%d", va_arg(args, int));
-if (format[i] == 'f')
-printf("%f", va_arg(args, double));
-if (format[i] == 's')
-{
-str = va_arg(args, char *);
-if (!str)
-printf("(nil)");
-else
-printf("%s", str);
-}
-}
-i++;
-}
-va_end(args);
-printf("\n");
+
+    va_start(args, format);
+
+    while (format && format[i])
+    {
+        if (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's')
+        {
+            if (printed)
+                printf(", ");
+
+            if (format[i] == 'c')
+                printf("%c", va_arg(args, int));
+            if (format[i] == 'i')
+                printf("%d", va_arg(args, int));
+            if (format[i] == 'f')
+                printf("%f", va_arg(args, double));
+            if (format[i] == 's')
+            {
+                str = va_arg(args, char *);
+                if (!str)
+                    printf("(nil)");
+                else
+                    printf("%s", str);
+            }
+            printed = 1;
+        }
+        i++;
+    }
+
+    va_end(args);
+    printf("\n");
 }
