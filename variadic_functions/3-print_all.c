d693b877b74d8678ce6aef7d3ad925f9dc690ff0
11968-ship-it <11968@holbertonstudents.com>
2025-09-28 14:22:39 +0300

    Update 3-print_all.c

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -10,44 +10,40 @@
  * 's' = string
  *
  * Description: Prints (nil) if string is NULL.
- * Prints a newline at the end. Ignores unknown format characters.
+ * Prints a newline at the end. Ignores unknown format characters
  */
 void print_all(const char * const format, ...)
 {
-	va_list args;
-	int i = 0;
-	char *str;
-	char *sep = "";
-
-	va_start(args, format);
-
-	while (format && format[i])
-	{
-		if (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's')
-		{
-			printf("%s", sep);
-
-			switch (format[i])
-			{
-				case 'c':
-					printf("%c", va_arg(args, int));
-					break;
-				case 'i':
-					printf("%d", va_arg(args, int));
-					break;
-				case 'f':
-					printf("%f", va_arg(args, double));
-					break;
-				case 's':
-					str = va_arg(args, char *);
-					printf("%s", str ? str : "(nil)");
-					break;
-			}
-			sep = ", ";
-		}
-		i++;
-	}
-
-	va_end(args);
-	printf("\n");
+va_list args;
+int i = 0;
+char *str;
+char *sep = "";
+va_start(args, format);
+while (format && format[i])
+{
+if (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's')
+{
+printf("%s", sep);
+switch (format[i])
+{
+case 'c':
+printf("%c", va_arg(args, int));
+break;
+case 'i':
+printf("%d", va_arg(args, int));
+break;
+case 'f':
+printf("%f", va_arg(args, double));
+break;
+case 's':
+str = va_arg(args, char *);
+printf("%s", str ? str : "(nil)");
+break;
+}
+sep = ", ";
+}
+i++;
+}
+va_end(args);
+printf("\n");
 }
