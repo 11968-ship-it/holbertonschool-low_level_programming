215012732d2de3060338dfd4df41d0d335849406
11968-ship-it <11968@holbertonstudents.com>
2025-09-28 13:38:11 +0300

    Update 3-print_all.c

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -12,75 +12,6 @@
  * Description: Prints (nil) if string is NULL.
  * Prints a newline at the end. Ignores unknown format characters.
  */
-int _putchar(char c);
-void print_str(char *str)
-{
-    int i = 0;
-
-    if (!str)
-    {
-        char *nil = "(nil)";
-        while (nil[i])
-        {
-            _putchar(nil[i]);
-            i++;
-        }
-        return;
-    }
-
-    while (str[i])
-    {
-        _putchar(str[i]);
-        i++;
-    }
-}
-void print_int(int n)
-{
-    unsigned int num;
-
-    if (n < 0)
-    {
-        _putchar('-');
-        num = -n;
-    }
-    else
-        num = n;
-
-    if (num / 10)
-        print_int(num / 10);
-
-    _putchar((num % 10) + '0');
-}
-void print_float(double f)
-{
-    int int_part = (int)f;
-    double frac_part = f - int_part;
-    int i;
-
-    if (f < 0)
-    {
-        _putchar('-');
-        int_part = -int_part;
-        frac_part = -frac_part;
-    }
-
-    print_int(int_part);
-    _putchar('.');
-    if (frac_part < 0)
-        frac_part = -frac_part;
-
-    frac_part *= 1000000;
-
-    int frac_int = (int)(frac_part + 0.5);
-    i = 100000;
-    while (i > 1 && frac_int / i == 0)
-    {
-        _putchar('0');
-        i /= 10;
-    }
-    print_int(frac_int);
-}
-
 void print_all(const char * const format, ...)
 {
     va_list args;
@@ -94,28 +25,26 @@ void print_all(const char * const format, ...)
         if (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's')
         {
             if (printed)
-            {
-                _putchar(',');
-                _putchar(' ');
-            }
-
+                printf(", ");
             if (format[i] == 'c')
-                _putchar(va_arg(args, int));
+                printf("%c", va_arg(args, int));
             if (format[i] == 'i')
-                print_int(va_arg(args, int));
+                printf("%d", va_arg(args, int));
             if (format[i] == 'f')
-                print_float(va_arg(args, double));
+                printf("%f", va_arg(args, double));
             if (format[i] == 's')
             {
                 str = va_arg(args, char *);
-                print_str(str);
+                if (!str)
+                    printf("(nil)");
+                else
+                    printf("%s", str);
             }
-
             printed = 1;
         }
         i++;
     }
-
     va_end(args);
-    _putchar('\n');
+    printf("\n");
 }
+
