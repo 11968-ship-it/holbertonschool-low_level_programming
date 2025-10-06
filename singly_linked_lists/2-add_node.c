9637cfdcbca47b7576bc4e3cf6afd77956d31c63
11968-ship-it <11968@holbertonstudents.com>
2025-10-06 19:47:51 +0300

    Update 2-add_node.c

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -3,6 +3,20 @@
 #include <string.h>
 /*strdup*/
 #include "lists.h"
+
+/**
+ * _strlen - calculates the length of a string
+ * @s: input string
+ *
+ * Return: length of the string (unsigned int)
+ */
+unsigned int _strlen(const char *s)
+{
+unsigned int len = 0;
+while (s[len] != '\0')
+len++;
+return len;
+}
 /**
  * add_node - adds a new node at the beginning of a list_t list
  * @head: pointer to pointer to the head of the list
@@ -21,7 +35,7 @@ if (new_node->str == NULL)
 free(new_node);
 return (NULL);
 }
-new_node->len = strlen(str);
+new_node->len = _strlen(str);
 new_node->next = *head;
 *head = new_node;
 return (new_node);
