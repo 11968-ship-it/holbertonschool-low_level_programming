35d647af63203124b2ea72e006d68e8260bb9ae5
11968-ship-it <11968@holbertonstudents.com>
2025-10-07 19:02:41 +0300

    Create 100-first.c

---
+++ singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * print_message - prints a message before main is executed
+ *
+ * Description: This function is executed before main due to the
+ * constructor attribute. It prints a fixed string.
+ */
+void print_message(void) __attribute__((constructor));
+void print_message(void)
+{
+printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
