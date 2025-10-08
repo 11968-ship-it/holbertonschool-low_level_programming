f07879402914eebd6f4903c21af1db989a12cfc3
11968-ship-it <11968@holbertonstudents.com>
2025-10-08 19:20:29 +0300

    Update 1-dlistint_len.c

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -1,1 +1,22 @@
-
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * dlistint_len - counts the number of elements in a doubly linked list
+ * @h: pointer to the head of the list
+ *
+ * Return: number of nodes in the list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+/*to intialize count to 0*/
+size_t count = 0;
+/*to loop until the end of the list */
+while (h != NULL)
+{
+/*count current node*/
+count++;
+h = h->next;
+}
+return (count);
+}
