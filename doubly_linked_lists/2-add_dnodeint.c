6455a6a395d799a34a5b455d1815fc47c4cf7776
11968-ship-it <11968@holbertonstudents.com>
2025-10-08 19:48:15 +0300

    Update 2-add_dnodeint.c

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -16,7 +16,7 @@ return (NULL);
 new_node->n = n;
 new_node->prev = NULL;
 new_node->next = *head;
-if (*head != NULL);
+if (*head != NULL)
 (*head)->prev = new_node;
 *head = new_node;
 return (new_node);
