7f0e00e56d411a9c8d517b385a2059f26d18f09b
11968-ship-it <11968@holbertonstudents.com>
2025-10-13 18:44:08 +0300

    Update 4-free_dlistint.c

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -1,1 +1,19 @@
+#include <stdlib.h>
+#include "lists.h"
 
+/**
+ * free_dlistint - frees a doubly linked list
+ * @head: pointer to the head of the list to be freed
+ *
+ * Description: Traverses the list, frees each node to prevent memory leaks.
+ */
+void free_dlistint(dlistint_t *head)
+{
+dlistint_t *temp;
+while (head != NULL)
+{
+temp = head->next;
+free(head);
+head = temp;
+}
+}
