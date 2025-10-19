699cb233e5c5e998fd765f495cf1391f98c6425c
11968-ship-it <11968@holbertonstudents.com>
2025-10-19 17:41:25 +0300

    Update 3-cp.c

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -39,29 +39,38 @@ int main(int argc, char *argv[])
 
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
-{
-close(fd_from);
-error_exit(99, "Error: Can't write to %s\n", argv[2]);
-}
-while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
-{
-bytes_written = write(fd_to, buffer, bytes_read);
-if (bytes_written != bytes_read)
-{
-close(fd_from);
-close(fd_to);
-error_exit(99, "Error: Can't write to %s\n", argv[2]);
-}
-}
-if (bytes_read == -1)
-{
-close(fd_from);
-close(fd_to);
-error_exit(98, "Error: Can't read from file %s\n", argv[1]);
-}
-if (close(fd_from) == -1)
-error_exit(100, "Error: Can't close fd %d\n", argv[1]);
-if (close(fd_to) == -1)
-error_exit(100, "Error: Can't close fd %d\n", argv[2]);
-return (0);
+	{
+		close(fd_from);
+		error_exit(99, "Error: Can't write to %s\n", argv[2]);
+	}
+
+	/* Updated read loop with immediate read error check */
+	while (1)
+	{
+		bytes_read = read(fd_from, buffer, BUFFER_SIZE);
+		if (bytes_read == -1)
+		{
+			close(fd_from);
+			close(fd_to);
+			error_exit(98, "Error: Can't read from file %s\n", argv[1]);
+		}
+		if (bytes_read == 0)
+			break;
+
+		bytes_written = write(fd_to, buffer, bytes_read);
+		if (bytes_written == -1 || bytes_written != bytes_read)
+		{
+			close(fd_from);
+			close(fd_to);
+			error_exit(99, "Error: Can't write to %s\n", argv[2]);
+		}
+	}
+
+	if (close(fd_from) == -1)
+		error_exit(100, "Error: Can't close fd %d\n", fd_from);
+
+	if (close(fd_to) == -1)
+		error_exit(100, "Error: Can't close fd %d\n", fd_to);
+
+	return (0);
 }
