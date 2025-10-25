13dc013c40ece1252b08fd243df10f653b027e0c
11968-ship-it <11968@holbertonstudents.com>
2025-10-25 22:44:22 +0300

    Update 0-hash_table_create.c

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -2,7 +2,12 @@
 #include <stdio.h>
 #include "hash_tables.h"
 /**
-*/
+ * hash_table_create - Creates a hash table
+ * @size: The size of the array
+ *
+ * Return: Pointer to the newly created hash table
+ * or NULL if something went wrong
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 hash_table_t *table;
