854b061692737e2d092d178c25f96a630f9e4163
11968-ship-it <11968@holbertonstudents.com>
2025-10-25 22:05:03 +0300

    Update 0-hash_table_create.c

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,9 +1,26 @@
-#include <stlib.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include "hash_tables.h"
 /**
 */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-malloc 
+hash_table_t *table;
+/*check if size = 0*/
+if (size == 0)
+return (NULL);
+table = malloc(sizeof(hash_table_t));
+if (table == NULL)
+return (NULL);
+table->size =  size;
+table->array = malloc(sizeof(hash_node_t *) * size);
+if (table->array == NULL)
+{
+free (table);
+return (NULL);
+}
+unsigned long int i;
+for (i = 0; i < size; i++)
+table->array[i] = NULL;
+return (table);
 }
