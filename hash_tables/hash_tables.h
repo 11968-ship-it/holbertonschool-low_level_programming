09edc6371a15f070dc74236662ff8d2fce7a5e42
11968-ship-it <11968@holbertonstudents.com>
2025-10-27 12:33:01 +0300

    Update hash_tables.h

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -37,5 +37,20 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
+typedef struct shash_node_s
+{
+char *key;
+char *value;
+struct shash_node_s *next;
+struct shash_node_s *sprev;
+struct shash_node_s *snext;
+} shash_node_t;
+typedef struct shash_table_s
+{
+unsigned long int size;
+shash_node_t **array;
+shash_node_t *shead;
+shash_node_t *stail;
+} shash_table_t;
 
 #endif
